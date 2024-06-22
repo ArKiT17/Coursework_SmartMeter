f1d7dfbb9bb49da4b69deca153576693585d4c3a
maxlikarenko <maxlikarenko@gmail.com>
2024-06-22 10:39:35 +0200

    - додані шалони сторінок для переходу

---
+++ app/src/main/java/com/smartmeter/activities/EditValueActivity.java
@@ -0,0 +1,21 @@
+package com.smartmeter.activities;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.smartmeter.R;
+
+public class EditValueActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_value);
+
+    }
+}
